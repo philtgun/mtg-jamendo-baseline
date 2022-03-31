415e91f3b4577c910c9a7c60ade5998bd224c6f5
Philip Tovstogan <philip.tovstogan@upf.edu>
2022-03-31 14:56:11 +0200

    Add function to average the results

--- src/results.py
+++ src/results.py
@@ -1,8 +1,17 @@
 import csv
+from pathlib import Path
+
+import pandas as pd
 
 
 # Intentionally not using pandas here, so it will work with whatever values are
-def add_to_output(path: str, values: list) -> None:
-    with open(path, 'a') as fp:
+def add_to_output(path: Path, values: list) -> None:
+    with path.open('a') as fp:
         writer = csv.writer(fp)
         writer.writerow(values)
+
+
+def parse_output(path: Path) -> None:
+    df = pd.read_csv(path, header=None)
+    print(df.groupby(1).mean())
+    print(df.groupby(1).std())
